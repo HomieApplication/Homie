2c2f19a099c1743a0f2243437ce5527f52efebd4
kubijaku <kubijaku11@gmail.com>
2022-10-23 22:20:44 +0200

    adding boxes to main screen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,10 +1,14 @@
 import React from 'react'; 
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 const MainScreen = () => {
     return(
         <View style={styles.container}>
             <Text>To jest MainScreen! Tu będą wszytskie ogłoszenia! 1 strona po zalogowaniu</Text>
+            <TextInput value=' ' placeholder = "username" style = {styles.textboxes} ></TextInput>
+            <TextInput value=' ' placeholder = "password" style = {styles.textboxes} ></TextInput>
+
         </View>
     )
 }
@@ -18,4 +22,12 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    textboxes: {
+        width: '90%',
+        fontSize: '18',
+        padding: '12',
+        borderColor: 'grey',
+        borderWidth: "0.2",
+        borderRadius: '10',
+    }
   });
