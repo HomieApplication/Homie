6dcfa9c917ca15a0fa32bbe7e278838e8dae3e6f
kubijaku <kubijaku11@gmail.com>
2022-10-23 22:38:58 +0200

    adding entryboxes to main screen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -7,7 +7,7 @@ const MainScreen = () => {
         <View style={styles.container}>
             <Text>To jest MainScreen! Tu będą wszytskie ogłoszenia! 1 strona po zalogowaniu</Text>
             <TextInput value=' ' placeholder = "username" style = {styles.textboxes} ></TextInput>
-            <TextInput value=' ' placeholder = "password" style = {styles.textboxes} ></TextInput>
+             <TextInput value=' ' placeholder = "password" style = {styles.textboxes} ></TextInput>
 
         </View>
     )
