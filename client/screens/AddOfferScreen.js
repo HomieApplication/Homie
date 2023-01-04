14a85f308a082367aa086e3b3252d722fbc96585
Eryk Mikołajek <mikolajek.eryk@gmail.com>
2023-01-04 17:52:07 +0100

    alpha1

--- client/screens/AddOfferScreen.js
+++ client/screens/AddOfferScreen.js
@@ -7,6 +7,10 @@ import {
   TextInput,
   useState,
   ActivityIndicator,
+  Alert,
+  Modal,
+  Pressable,
+  Image,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SearchableDropdown from "react-native-searchable-dropdown";
@@ -15,7 +19,8 @@ import axios from "axios";
 
 import { displayAlertBox } from "../components/alert";
 
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import marker from "../assets/icons8-marker.png";
 
 function LoadingAnimation() {
   return (
@@ -35,6 +40,7 @@ const AddOfferScreen = ({ navigation }) => {
   const [description, onChangeDescription] = React.useState("");
   const [localization, onChangeLocalization] = React.useState("");
   const [loading, setLoading] = React.useState(false);
+  const [modalVisible, setModalVisible] = React.useState(false);
 
   const sendData = () => {
     axios
@@ -73,22 +79,64 @@ const AddOfferScreen = ({ navigation }) => {
             value={description}
             placeholder="Description"
           />
+          {/*}
           <TextInput
             style={styles.textboxes}
             onChangeText={onChangeLocalization}
             value={localization}
             placeholder="Localization (city for now...)"
           />
+          */}
+          <View>
+            <Modal
+              animationType="slide"
+              visible={modalVisible}
+              transparent={true}
+              onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <View style={styles.modalView}>
+                <Text style={[styles.aboveMap]}>Choose a location on map</Text>
+                <MapView
+                  style={styles.map}
+                  showsUserLocation={true}
+                  provider={PROVIDER_GOOGLE}
+                  loadingEnabled
+                  showsCompass
+                  ref={(el) => (this.mapView = el)}
+                ></MapView>
+                <View style={styles.markerFixed}>
+                  <Image style={styles.marker} source={marker} />
+                </View>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.itemText}>Done</Text>
+                </Pressable>
+              </View>
+            </Modal>
+            <Pressable
+              style={[styles.button, styles.buttonOpen]}
+              onPress={() => setModalVisible(true)}
+            >
+              <Text style={styles.itemText}>Choose a location</Text>
+            </Pressable>
+          </View>
+          {/*
           <View style={styles.container}>
             <MapView
               style={styles.map}
-              showsUserLocation
+              showsUserLocation={true}
+              provider={PROVIDER_GOOGLE}
               loadingEnabled
               showsCompass
-              showsTraffic
               ref={(el) => (this.mapView = el)}
             ></MapView>
           </View>
+            */}
           <SignInBtn
             style={styles.button}
             title="Create offer"
@@ -179,31 +227,50 @@ const styles = StyleSheet.create({
   },
   map: {
     position: "absolute",
-    flex: 1,
+    bottom: 0,
     width: "100%",
-    height: "100%",
+    height: "95%",
+  },
+  modalView: {
+    width: "100%",
+    height: "92%",
+    position: "absolute",
+    justifyContent: "center",
+    bottom: 0,
+    paddingTop: 20,
+    marginTop: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonClose: {
+    position: "absolute",
+    bottom: 25,
+  },
+  aboveMap: {
+    position: "absolute",
+    top: 15,
+    color: "#000",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  markerFixed: {
+    left: "50%",
+    marginLeft: -24,
+    marginTop: -48,
+    position: "absolute",
+    top: "50%",
+  },
+  marker: {
+    height: 48,
+    width: 48,
   },
 });
-/*
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-});
-*/
