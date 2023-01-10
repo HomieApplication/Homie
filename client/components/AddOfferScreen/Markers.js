52accc0b417ac8bd3b5d05f2198e12de37648c9e
kubijaku <kubijaku11@gmail.com>
2023-01-10 12:16:08 +0100

    Ordnung muss sein

---
+++ client/components/AddOfferScreen/Markers.js
@@ -0,0 +1,162 @@
+export const markers = [
+    {
+    title: 'I DS Alfa',
+    coordinates: {
+      latitude : 50.065868409336915 ,
+      longitude : 19.915877276798597
+    },
+    identifier: 'I DS Alfa',
+  },
+  {
+    title: 'DS1 Olimp',
+    coordinates: {
+      latitude : 50.06909454120224 , 
+      longitude : 19.904426686576333,
+    },
+    identifier: 'DS1 Olimp',
+  },
+  {
+    title: 'DS2 Babilon',
+    coordinates: {
+      latitude : 50.06945669606302 , 
+      longitude : 19.905645376062772
+    },
+    identifier: 'DS2 Babilon',
+  },
+  {
+    title: 'DS3 Akropol',
+    coordinates: {
+      latitude : 50.06971744764018 , 
+      longitude : 19.903386380497768
+    },
+    identifier: 'DS3 Akropol',
+  },
+  {
+    title: 'DS4 Filutek',
+    coordinates: {
+      latitude : 50.069460793917536 , 
+      longitude : 19.9059155884422
+    },
+    identifier: 'DS4 Filutek',
+  },
+  {
+    title: 'DS5 Strumyk',
+    coordinates: {
+      latitude : 50.06905674103451 , 
+      longitude : 19.90610522467604
+    },
+    identifier: 'DS5 Strumyk',
+  },
+  {
+    title: 'DS6 Bratek',
+    coordinates: {
+      latitude : 50.06855879085213, 
+      longitude : 19.905613674949148
+    },
+    identifier: 'DS6 Bratek',
+  },
+  {
+    title: 'DS7 Zaścianek',
+    coordinates: {
+      latitude : 50.06817409779714, 
+      longitude : 19.905322959606522 
+    },
+    identifier: 'DS7 Zaścianek',
+  },
+  {
+    title: 'DS8 Stokrotka',
+    coordinates: {
+      latitude : 50.06772443916539, 
+      longitude : 19.905325603784718
+    },
+    identifier: 'DS8 Stokrotka',
+  },
+  {
+    title: 'DS9 Omega',
+    coordinates: {
+      latitude : 50.06919964379084 , 
+      longitude : 19.90736705166221
+    },
+    identifier: 'DS9 Omega',
+  },
+  {
+    title: 'DS10 Hajduczek',
+    coordinates: {
+      latitude : 50.06886565521682, 
+      longitude : 19.907048174949157
+    },
+    identifier: 'DS10 Hajduczek',
+  },
+  {
+    title: 'DS11 Bonus',
+    coordinates: {
+      latitude : 50.06838423746026, 
+      longitude : 19.907120007483826
+    },
+    identifier: 'DS11 Bonus',
+  },
+  {
+    title: 'DS12 Promyk',
+    coordinates: {
+      latitude : 50.068034058082404, 
+      longitude : 19.90635435960652
+    },
+    identifier: 'DS12 Promyk',
+  },
+  {
+    title: 'DS13 Straszny Dwór',
+    coordinates: {
+      latitude : 50.06748794624835, 
+      longitude : 19.906444676798568
+    },
+    identifier: 'DS13 Straszny Dwór',
+  },
+  {
+    title: 'DS14 Kapitol',
+    coordinates: {
+      latitude : 50.067863618615206, 
+      longitude : 19.90730764611346
+    },
+    identifier: 'DS14 Kapitol',
+  },
+  {
+    title: 'DS15 Maraton',
+    coordinates: {
+      latitude : 50.06825607072731, 
+      longitude : 19.902025292141232
+    },
+    identifier: 'DS15 Maraton',
+  },
+  {
+    title: 'DS16 Itaka',
+    coordinates: {
+      latitude : 50.06872718264994, 
+      longitude : 19.902064476798696
+    },
+    identifier: 'DS16 Itaka',
+  },
+  {
+    title: 'DS17 Arkadia',
+    coordinates: {
+      latitude : 50.06908944917313, 
+      longitude : 19.902272932620487
+    },
+    identifier: 'DS17 Arkadia',
+  },
+  {
+    title: 'DS18 Odyseja',
+    coordinates: {
+      latitude : 50.06954129586869, 
+      longitude : 19.902069605444794
+    },
+    identifier: 'DS18 Odyseja',
+  },
+  {
+    title: 'DS19 Apollo',
+    coordinates: {
+      latitude : 50.06996436069946, 
+      longitude : 19.90235311727785
+    },
+    identifier: 'DS19 Apollo',
+  },
+];
